6ecb82cfd2c03f8f18e5410a0d54b3e39f17fcd2
PraSarOG <pratyay.sarkar17@gmail.com>
2022-05-13 22:20:05 +0530

    build updated

---
+++ build/precache-manifest.4227313f8d6b4a1adcf302b697f212ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c079cc15117855020d7131d833f99e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64a0ded1298319c96aef",
+    "url": "/static/css/2.89d568a6.chunk.css"
+  },
+  {
+    "revision": "64a0ded1298319c96aef",
+    "url": "/static/js/2.d1b89373.chunk.js"
+  },
+  {
+    "revision": "83542687243be86a7588",
+    "url": "/static/js/main.489defe6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
